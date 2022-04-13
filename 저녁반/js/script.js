d9fd25e246378ef468179f99c235bdc1d4d9ea60
nigrojin <nigrojin@naver.com>
2022-04-13 21:53:13 +0900

    4/13

--- 저녁반/js/script.js
+++ 저녁반/js/script.js
@@ -240,15 +240,15 @@ hello world
 // Object and Array
 
 // Object
-var car = {
-    // key : value
-    name: '제네시스',
-    year: 2022,
-    color: '검정',
-    sound: function () { // value가 함수인 것을 Method (메서드)라고 한다
-        return '부웅'
-    }
-}
+// var car = {
+//     // key : value
+//     name: '제네시스',
+//     year: 2022,
+//     color: '검정',
+//     sound: function () { // value가 함수인 것을 Method (메서드)라고 한다
+//         return '부웅'
+//     }
+// }
 // // name, year, color = property (속성)
 
 // console.log(car)
@@ -278,12 +278,12 @@ var car = {
 // // Template literal
 // // String (문자)안에서 변수를 사용할 수 있다
 // // ``
-for (var i=0; i<cars.length; i++) { // i = index
-    // console.log(
-    //     "자동차 이름: " + cars[i].name + ", 디자인: " + cars[i].design + "점"
-    // )
-    console.log(`자동차 이름: ${cars[i].name}, 디자인: ${cars[i].design}점`)
-}
+// for (var i=0; i<cars.length; i++) { // i = index
+//     // console.log(
+//     //     "자동차 이름: " + cars[i].name + ", 디자인: " + cars[i].design + "점"
+//     // )
+//     console.log(`자동차 이름: ${cars[i].name}, 디자인: ${cars[i].design}점`)
+// }
 
 // console.log(cars[0].design)
 
@@ -302,6 +302,302 @@ for (var i=0; i<cars.length; i++) { // i = index
 
 // // 제네시스의 화이트 색상에 접근해보기
 // console.log(cars[0].color[1]);
+
+// DATA TYPE
+// String (문자)
+// Number (숫자)
+// Boolean (true or false)
+// undefined
+// null
+
+// var fruit = 'apple'
+// toUpperCase 대문자로 변환
+// var result = fruit.toUpperCase();
+
+// var fruit = 'APPLE';
+// toLowerCase() 소문자로 변환
+// var result = fruit.toLowerCase();
+
+// console.log(result);
+// console.log(result.length);
+
+// var x = 20
+// var y = "22"
+
+// // Number() 문자를 숫자로 변환
+// y = Number(y);
+
+// // toString() 숫자를 문자로 변환 
+// y = y.toString();
+
+// console.log(typeof (x + y)) // string
+// console.log(x + y);
+
+// var cars = ["제네시스", "스타리아"]
+
+// console.log(cars)
+
+// // push(value) 새로운 value 추가
+// cars.push("아이오닉")
+// cars.push("캐스퍼")
+
+// console.log(cars)
+// console.log(cars.length)
+
+// // pop(value) value 제거 (가장 마지막)
+// cars.pop()
+
+// console.log(cars)
+
+// // includes(value) vale를 포함했는지 여부, true or false
+// var bool = cars.includes("캐스퍼");
+
+// console.log(bool);
+
+// Function and Method (함수와 메서드)
+
+
+// Object
+// var car = {
+//     // key: value
+//     name: '제네시스', // property (속성)
+//     onSale: true, // property (속성)
+//     sound: function () { // method (메서드)
+//         return '부릉'
+//     }
+// }
+
+// // Method and Function
+// // Method는 객체에 속해있다
+
+// console.log(car.name)
+// console.log(car.onSale)
+// // Method (메서드) 
+// console.log(car.sound())
+
+// // Function (함수)
+// hello()
+// window.hello()
+
+// console.log(window)
+
+// function hello() {
+//     console.log('hello world')
+// }
+
+// var fruit = 'apple'
+
+// // toUpperCase()
+// var result = fruit.toUpperCase();
+
+// console.log(result)
+
+// // result의 프로토타입 객체에 접근 
+// console.log(result.__proto__)
+
+// var num = 0
+
+// console.log(num.__proto__)
+
+// Function
+
+// Function declaration, expression (함수선언식, 함수표현식)
+
+// Declaration 함수선언식
+// function hello() {
+//     console.log("hello")
+// }
+
+// Expression 함수표현식
+// 변수에 익명함수를 담는다
+// var hello = function() {
+//     console.log('hello')
+// }
+// hello()
+
+// f()
+// Hoisting (게양)
+// 함수의 정의는 호출보다 올라간다
+// function f() {
+//     console.log('hello')
+// }
+
+// expression 함수표현식은 hoisting이 되지 않는다
+// f() // error
+// var f = function () {
+//     console.log('hello')
+// }
+// f() // ok
+
+// return
+
+// f();
+// function f() {
+//     // console.log('hello')
+//     // return;
+//     // console.log('world');
+
+//     return console.log('hello');
+//     console.log('world')
+// }
+
+// parameter (인자)
+
+// function f(a, b) {
+//     return a + b
+// }
+
+// var result = f(20, 22)
+
+// console.log(result)
+
+// function isAdult(age) {
+//     if (age >= 18) {
+//         return '성인'
+//     } else {
+//         return '청소년 또는 어린이'
+//     }
+// }
+
+// var a = isAdult(20)
+// var b = isAdult(10)
+
+// console.log(a)
+// console.log(b)
+
+// var fruits = ["apple", "banana", "orange", "mango"];
+
+// var result = [];
+
+// for (i=0; i<fruits.length; i++) { // i = index
+//     result.push(fruits[i].toUpperCase())
+// }
+
+// console.log(result)
+
+// Callback Function (콜백 함수)
+// 다른 함수의 인자가 되는 함수
+
+
+// function myFunction(cb) {
+//     console.log(1)
+//     cb()
+// }
+
+// function myCallback() {
+//     console.log(2)
+// }
+
+// // 콜백함수에서 ()는 제거한다
+// myFunction(myCallback)
+
+// function myFunction(cb) {
+//     console.log('hello')
+//     cb('world')
+// }
+
+// function myCallback(data) {
+//     console.log(data)
+// }
+
+// // myFunction(myCallback(data)) // error
+// myFunction(function (data) { // ok
+//     myCallback(data)
+// })
+
+// function myFunction(cb) {
+//     console.log(1)
+//     cb()
+// }
+
+// myFunction(function() {
+//     console.log(2)
+// })
+
+// function myFunction(cb) {
+//     console.log(1)
+//     cb()
+// }
+
+// function myCallback(data) {
+//     console.log(data)
+// }
+// // 콜백함수에 인자가 있을 때는 익명함수가 필요하다
+
+// // myFunction(myCallback(2)) // error
+
+// myFunction(function () {
+//     myCallback(2)
+// })
+
+
+// forEach, filter
+
+// var fruits = ['apple', 'banana', 'orange'];
+// var result = [];
+
+// // forEach(callback) Array의 길이만큼 callback 함수가 반복 실행된다
+// fruits.forEach(function (value) {
+//     result.push(value.toUpperCase())
+// })
+
+// console.log(result)
+
+// var ages = [10, 20, 30];
+
+// // filter() 조건에 맞는 새로운 Array를 return한다
+// var result = ages.filter(function (value) {
+//     if (value > 18) {
+//         return value;
+//     }
+// })
+
+// console.log(result)
+
+
+// 판매중인 자동차의 Array를 만들어보세요
+var cars = [
+    { name: '제네시스', onSale: true },
+    { name: '스타리아', onSale: true },
+    { name: '아이오닉', onSale: true },
+    { name: '포니', onSale: false },
+    { name: '캐스퍼', onSale: true },
+];
+
+
+
+var result = cars.filter(function (value) {
+    if (value.onSale === true) {
+        return value;
+    }
+})
+
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
